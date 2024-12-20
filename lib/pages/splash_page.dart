9300705369130363d596794497b0f74eb71a5a81
Shana2002 <hansakaravi02@gmail.com>
2024-12-20 22:58:28 +0530

    Splash page logo and defult fonts setup

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:zapbill/services/navigation_service.dart';
 
 class SplashPage extends StatefulWidget {
   final VoidCallback onInitilizationComplete;
@@ -13,14 +16,37 @@ class _SplashPageState extends State<SplashPage> {
   void initState() {
     super.initState();
     Future.delayed(Duration(seconds: 3)).then((_) {
-
+      _setup().then((_) => widget.onInitilizationComplete());
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "zapBill",
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/logo.png"),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
-  
+  Future<void> _setup() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    // await Firebase.initializeApp();
+    _registerServices();
+  }
+
+  void _registerServices() {
+    GetIt.instance.registerSingleton<NavigationService>(NavigationService());
+  }
 }
