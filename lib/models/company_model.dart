9ff78ef4c6b4aea6a11b918b425a2812cb0ffd43
Shana2002 <hansakaravi02@gmail.com>
2024-12-21 09:59:35 +0530

    Company Model done

--- lib/models/company_model.dart
+++ lib/models/company_model.dart
@@ -1,7 +1,32 @@
-// class CompanyModel {
-//   final String userName;
-//   final String email;
-//   final String companyName;
-//   final String companyEmail;
-//   final String companyMobile;
-// }
+class CompanyModel {
+  final String userName;
+  final String email;
+  final String companyName;
+  final String companyEmail;
+  final String companyMobile;
+  CompanyModel(
+      {required this.userName,
+      required this.email,
+      required this.companyName,
+      required this.companyEmail,
+      required this.companyMobile});
+
+  factory CompanyModel.fromJSON(Map<String, dynamic> _json) {
+    return CompanyModel(
+        userName: _json["userName"],
+        email: _json["email"],
+        companyName: _json["companyName"],
+        companyEmail: _json["companyEmail"],
+        companyMobile: _json["companyMobile"]);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "userName": userName,
+      "email": email,
+      "companyName": companyName,
+      "companyEmail": companyEmail,
+      "companyMobile": companyMobile
+    };
+  }
+}
